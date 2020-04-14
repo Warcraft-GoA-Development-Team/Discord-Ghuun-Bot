c314e8ba7a705fd193fb97e36d83b36922345d02
DukeJeans <nicholasjeans@gmail.com>
2020-04-14 19:14:44 -0400

    Completed migration

--- js/media-service.js
+++ js/media-service.js
@@ -1,14 +1,14 @@
-const Discord = require('discord.js');
+const { MessageAttachment } = require('discord.js');
 
-exports.knightVideo = new Discord.Attachment('../videos/knight.mp4');
-exports.turtleVideo = new Discord.Attachment('../videos/turtle.mp4');
-exports.whip = new Discord.Attachment('../videos/whip.mp4');
-exports.wellMet = new Discord.Attachment('../images/wellmet.png');
-exports.khalid = new Discord.Attachment('../images/khalid.gif');
-exports.ghuun1 = new Discord.Attachment('../images/ghuun1.png');
-exports.ghuun2 = new Discord.Attachment('../images/ghuun2.png');
-exports.ghuun3 = new Discord.Attachment('../images/ghuun3.png');
-exports.ghuun4 = new Discord.Attachment('../images/ghuun4.png');
-exports.ghuun5 = new Discord.Attachment('../images/ghuun5.png');
-exports.ghuun6 = new Discord.Attachment('../images/ghuun6.png');
-exports.geralt = new Discord.Attachment('../images/geralt.png');
+exports.knightVideo = new MessageAttachment('./videos/knight.mp4');
+exports.turtleVideo = new MessageAttachment('./videos/turtle.mp4');
+exports.whip = new MessageAttachment('./videos/whip.mp4');
+exports.wellMet = new MessageAttachment('./images/wellmet.png');
+exports.khalid = new MessageAttachment('./images/khalid.gif');
+exports.ghuun1 = new MessageAttachment('./images/ghuun1.png');
+exports.ghuun2 = new MessageAttachment('./images/ghuun2.png');
+exports.ghuun3 = new MessageAttachment('./images/ghuun3.png');
+exports.ghuun4 = new MessageAttachment('./images/ghuun4.png');
+exports.ghuun5 = new MessageAttachment('./images/ghuun5.png');
+exports.ghuun6 = new MessageAttachment('./images/ghuun6.png');
+exports.geralt = new MessageAttachment('./images/geralt.png');
